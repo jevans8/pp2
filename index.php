ab8730c1963dea77afa591e7ac8e97eacc661e69
Julia Evans <jevans8@mail.greenriver.edu>
2020-04-17 16:12:46 -0400

    Added index page

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Julia Evans
+ * 4/17/2020
+ * https://jevans.greenriverdev.com/328/pairPrograms/pp2/index.php
+ * Pair Program 2
+*/
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?php
+
+//1.
+echo "PHP Array Practice";
+
+//2.
+
+
+
+
+?>
+</body>
+</html>
